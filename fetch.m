f074f87b769d29882a1ee3fb8374d22cacda6127
xero <volleyp7689@gmail.com>
2015-04-23 21:13:41 +0800

    Fixed Conflict

--- fetch.m
+++ fetch.m
@@ -5,41 +5,26 @@ function [ data, fetch_success ] = fetch(taifex_url)
     % The opening time of futures exchange is 8:35 ~ 13:45,
     % Sleep the process until that time.
     current = clock;
-    %{
     if current(4) < 8 || current(4) == 8 && current(5) < 35
         current_sec = current(4) * 60 * 60 + current(5) * 60 + current(6);
         open_sec = 8 * 60 * 60 + 35 * 60;
-<<<<<<< HEAD
-        disp('Zzz..');
-        pause(open_sec - current_sec);
-        finish_flag = false;
-        data = 0;
-=======
         fprintf('Pause Process..\n');
         pause(open_sec - current_sec);
         fetch_success = 0;
         data = '';
->>>>>>> origin/master
         return;
     end;
     if current(4) > 13 || current(4) ==13 && current(4) > 55
         current_sec = current(4) * 60 * 60 + current(5) * 60 + current(6);
         one_day_in_sec = 24 * 60 * 60;
         open_sec = 8 * 60 * 60 + 35 * 60;
-<<<<<<< HEAD
-        disp('Zzz..');
-        pause(one_day_in_sec + (open_sec - current_sec));
-        finish_flag = false;
-        data = 0;
-=======
         fprintf('Pause Process..\n');
         pause(one_day_in_sec + (open_sec - current_sec));
         fetch_success = 0;
         data = '';
->>>>>>> origin/master
         return
     end;
-    %}
+    
     
     % Download Taifex source page
     % Some url error occured while fetching.
--- main.m
+++ main.m
@@ -9,14 +9,9 @@ clc;
 taifex_url = 'http://info512.taifex.com.tw/Future/FusaQuote_Norl.aspx';
 latest_data = {};
 data_path = [pwd() '/data'];
-<<<<<<< HEAD
-period = 0.5; % 1ms
-retry = 10;
-=======
 period = 1; % 1 second
 %retry = 10;
 FINISH = false;
->>>>>>> origin/master
 
 fprintf('Launch Crawler..');
 
@@ -25,17 +20,14 @@ while ~FINISH
     [data, fetch_status] = fetch(taifex_url);
 
     % Output
-<<<<<<< HEAD
-    os = output(data, data_path);
+    output(data, data_path);
 
     % sleep period
-=======
     if fetch_status
         output(data, data_path);
     end;
     
     % Sleep
->>>>>>> origin/master
     if period
         pause(period);
     end;
