9843328911a350217087738a3142e66d62351c67
xero <volleyp7689@gmail.com>
2015-04-29 14:44:27 +0800

    new comment to main

--- main.m
+++ main.m
@@ -1,9 +1,11 @@
 % Taifex Real Time Crawler
 % author:xero7689
 % email:volleyp7689@gmail.com
-
 clear all;
 clc;
 
 % Sample of using taifex_realTime Function
+% The Arguments is a 1d matrix which contains the id of futures.
+% [0]: Will fetch every futures.
+% [1, 2, 3...]: Will only fetch future 1 ~ 3. 
 taifex_realTime([1, 2, 3]);
