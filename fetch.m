de2989495c812dadd47bdad9baeb83fd52d3a373
xero <volleyp7689@gmail.com>
2015-05-01 10:38:27 +0800

    Fixed a bug inside time-stamp checking.

--- fetch.m
+++ fetch.m
@@ -5,7 +5,8 @@ function [ data, start_time, finish_time, fetch_success ] = fetch(taifex_url)
     % The opening time of futures exchange is 8:35 ~ 13:45,
     % Sleep the process until that time.
     current = clock;
-
+    start_time = current;
+    
     if current(4) < 8 || current(4) == 8 && current(5) < 35
         current_sec = current(4) * 60 * 60 + current(5) * 60 + current(6);
         open_sec = 8 * 60 * 60 + 35 * 60;
@@ -13,6 +14,7 @@ function [ data, start_time, finish_time, fetch_success ] = fetch(taifex_url)
         pause(open_sec - current_sec);
         fetch_success = 0;
         data = '';
+        finish_time = clock;
         return;
     end;
     if current(4) > 13 || current(4) ==13 && current(4) > 55
@@ -23,12 +25,15 @@ function [ data, start_time, finish_time, fetch_success ] = fetch(taifex_url)
         pause(one_day_in_sec + (open_sec - current_sec));
         fetch_success = 0;
         data = '';
+        finish_time = clock;
         return
     end;
+    
     
     % Download Taifex source page
     % Some url error occured while fetching.
     start_time = clock; 
+    source_page = '';
     try
         source_page = urlread(taifex_url);
     catch 
--- main.m
+++ main.m
@@ -8,4 +8,4 @@ clc;
 % The Arguments is a 1d matrix which contains the id of futures.
 % [0]: Will fetch every futures.
 % [1, 2, 3...]: Will only fetch future 1 ~ 3. 
-taifex_realTime([1, 2, 3]);
+taifex_realTime([0]);
